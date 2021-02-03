27d681a07d9a0220761c6d38c0270533fd98706d
Tom <mcmahon91@icloud.com>
2021-02-03 22:03:26 +0000

    finished basics

--- project36second/GameViewController.swift
+++ project36second/GameViewController.swift
@@ -18,7 +18,8 @@ class GameViewController: UIViewController {
             // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
                 // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
+                scene.scaleMode = .aspectFill;
+//                scene.scaleMode = .aspectFill;
                 
                 // Present the scene
                 view.presentScene(scene)
@@ -28,6 +29,7 @@ class GameViewController: UIViewController {
             
             view.showsFPS = true
             view.showsNodeCount = true
+            view.showsPhysics = true
         }
     }
 
